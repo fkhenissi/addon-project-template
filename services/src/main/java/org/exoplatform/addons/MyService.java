e8437145741145417d7995e350f0da1d0e7a10fe
Fehmy Khenissy <fkhenissi@exoplatform.com>
2018-09-20 10:29:44 +0100

    new service

--- services/src/main/java/org/exoplatform/addons/MyService.java
+++ services/src/main/java/org/exoplatform/addons/MyService.java
@@ -1,9 +1,26 @@
 package org.exoplatform.addons;
 
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.picocontainer.Startable;
 /**
  * Created by eXo Platform SAS.
  *
  */
 
-public class MyService {
+public class MyService implements Startable {
+
+    private static final Log LOG = ExoLogger.getLogger(MyService.class);
+
+
+
+    @Override
+    public void start() {
+        LOG.info("Service is started by @fkhenissy");
+    }
+
+    @Override
+    public void stop() {
+        LOG.error("Service was stopped by @fkhenissy");
+    }
 }
